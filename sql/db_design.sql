e42a20786c553d8401d07938b2d064640d3c1cdc
Abrash Mikhail <bastazf@mail.ru>
2020-12-01 04:01:34 +0300

    separeated creating constraints for tables

--- sql/db_design.sql
+++ sql/db_design.sql
@@ -44,24 +44,3 @@ CREATE TABLE public.workload (
                 CONSTRAINT workload_pk PRIMARY KEY (workload_id)
 );
 
-
-ALTER TABLE public.workload ADD CONSTRAINT job_workload_fk
-FOREIGN KEY (job_id)
-REFERENCES public.job (job_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.job_schedule ADD CONSTRAINT job_job_schedule_fk
-FOREIGN KEY (job_id)
-REFERENCES public.job (job_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.workload ADD CONSTRAINT employee_workload_fk
-FOREIGN KEY (employee_id)
-REFERENCES public.employee (employee_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
