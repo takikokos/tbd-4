60a27d1eede6014a3778933a65cf751ec7731add
Abrash Mikhail <bastazf@mail.ru>
2020-12-03 23:04:28 +0300

    added create_views.sql to list of init scripts

--- init_load_db.py
+++ init_load_db.py
@@ -6,5 +6,5 @@ from db_api.postgres_executor import PostgresExecutor
 
 
 ex = PostgresExecutor("./dev_postgres_conn.conf.json")
-for init_script_name in ["./sql/db_design.sql", "./sql/load_mocks.sql", "./sql/create_constraints.sql"]:
+for init_script_name in ["./sql/db_design.sql", "./sql/load_mocks.sql", "./sql/create_constraints.sql", "./sql/create_views.sql"]:
     ex.execute_script_file(init_script_name)
